71e3f4607f71f0ec3a92949dec84df0b5e00675e
Jyoti <jyotirmayee.kumar@ibm.com>
2022-02-24 17:19:08 +0530

    update the main method declaration with return type new

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -19,7 +19,8 @@ public class Greeter {
   }
 
   public static void main(String args[]){
-    System.out.println("Hello from Main Method!!!!");
+
+      System.out.println("Hello from Main Method!!!!");
     
   }
 }
