d77f9b6d4c8dc03a3e205d116fd25eb6a4ab9a27
Jyoti <jyotirmayee.kumar@ibm.com>
2022-02-24 14:15:40 +0530

    Test code changed with one new function

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -44,4 +44,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+     @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Hello World, Thanks you making me more Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
