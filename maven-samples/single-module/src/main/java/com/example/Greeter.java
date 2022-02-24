046521fe12e2174725da5e2a3c4faa3c15468310
Jyoti <jyotirmayee.kumar@ibm.com>
2022-02-24 16:22:35 +0530

    update the main method declaration

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -17,7 +17,7 @@ public class Greeter {
     return String.format("Hello, %s!", someone);
   }
 
-  public String void main (String args[]) {
-    system.out.println("Hello from Main Method!!!!");
+  public static void main (String args[]) {
+    System.out.println("Hello from Main Method!!!!");
   }
 }
