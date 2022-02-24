f12cf87772f30cae2b0f6993cc396e9919fc57f8
Jyoti <jyotirmayee.kumar@ibm.com>
2022-02-24 18:13:04 +0530

    update the main method declaration with return type new

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+  //public String greet(String someone) {
+    //return String.format("Hello, %s!", someone);
+  //}
 
    public static void main(String args[]) {
         System.out.println("Hello, World!"); 
