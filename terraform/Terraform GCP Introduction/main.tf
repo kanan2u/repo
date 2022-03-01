4ef78bd92f54c9d57498286f48d62aef78e18b45
Jyoti <jyotirmayee.kumar@ibm.com>
2022-03-01 17:46:49 +0530

    main.tf file updated

--- terraform/Terraform GCP Introduction/main.tf
+++ terraform/Terraform GCP Introduction/main.tf
@@ -10,3 +10,11 @@ resource "google_compute_network" "vpc_network" {
     name                    = "first-demo-network"
     auto_create_subnetworks = "true"
 }
+
+terraform {
+  backend "gcs" {
+    bucket  = "levelup360-terraform"
+    prefix  = "terraform-practice"
+    credentials = "gold-verve-335308-5ec71036ae59.json"
+  }
+}
