aade7c8d745727aaef8ea31753acf34f8bf84cc8
Jyoti <jyotirmayee.kumar@ibm.com>
2022-03-01 16:57:34 +0530

    Terraform project files addded

---
+++ terraform/Terraform GCP Introduction/main.tf
@@ -0,0 +1,12 @@
+provider "google" {
+    credentials = file("gold-verve-335308-5ec71036ae59.json")
+
+    project = "gold-verve-335308"
+    region  = "us-east1"
+    zone    = "us-east1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+    name                    = "first-demo-network"
+    auto_create_subnetworks = "true"
+}
